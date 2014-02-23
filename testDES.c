7e4f92109d8ad21ba6721df6c939cde34706ca31
Dean Chester <dean@maquib.com>
2014-02-23 22:38:55 +0000

    Added decryption operation confirm result.

--- testDES.c
+++ testDES.c
@@ -7,8 +7,8 @@ int main (int argc, char const *argv[])
 	PLAIN_TEXT pt = 0; 
 	
 	CIPHER_TEXT ct = encrypt(pt, key);
-	
-	printf("Ciphered Value is: %llu\n", ct);
+	PLAIN_TEXT p = decrypt(ct, key);
+	printf("Ciphered Value is: %llu\n", p);
 	
 	return 0;
 }
