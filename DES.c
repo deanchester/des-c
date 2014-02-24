597e19609932291cae5eef8c59c7ee0a3842a4a6
Dean Chester <dean@maquib.com>
2014-02-24 10:53:23 +0000

    Fixed bracketing issue with shift left and shift right

--- DES.c
+++ DES.c
@@ -64,8 +64,8 @@ int IIP[] = {40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,38,6,46,14,54,22,
 			62,30,37,5,45,13,53,21,61,29,36,4,44,12,52,20,60,28,35,3,43,11,51,
 			19,59,27,34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25};
 
-inline KEY_LR shiftKey(KEY_LR key){
-	return ((key << 1) | (((key >> sizeof(key) * 8) - 1)));
+KEY_LR shiftKey(KEY_LR key){
+	return (key << (unsigned int)1) | (key >> (sizeof(key) * (unsigned int) 8 - (unsigned int) 1));
 }
 
 /*
@@ -75,14 +75,17 @@ void scheduleKeys(KEY key, KEY *keysP){
 	int i;
 	KEY keyPC1 = 0;
 	
-	for(i = 1; i<65; i++){
-		KEY tmpKeyMasked = ((65 - i) & key);
-		tmpKeyMasked << PC1[i];
-		keyPC1 += tmpKeyMasked;
-	}	
-	
-	KEY_LR key_left = ((keyPC1 & 0xFFFFFFFF00000000) >> 32);
-	KEY_LR key_right = (keyPC1 & 0x00000000FFFFFFFF);
+	KEY value = 1;
+	for(i = 0; i<64; i++){
+		KEY tmpKeyMasked = (value & key);
+		tmpKeyMasked <<= PC1[i];
+		value *= 2;
+		keyPC1 |= tmpKeyMasked;
+	}
+	KEY_LR key_left = 0;
+	KEY_LR key_right = 0;
+	key_left = ((keyPC1 & 0xFFFFFFFF00000000) >> 32);
+	key_right =  (keyPC1 & 0x00000000FFFFFFFF);
 
 	for(i = 0; i<16; i++){
 		if(i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7 || i == 9 || 
@@ -93,14 +96,15 @@ void scheduleKeys(KEY key, KEY *keysP){
 		
 		key_left = shiftKey(key_left);
 		key_right = shiftKey(key_right);
-		
-		KEY keyReformed = key_left << 32 & key_right; 
+
+		KEY keyReformed = (key_left << 32) | key_right; 
+
 		KEY keyPC2 = 0;
-		int j; 
-		for(j = 1; j<65; j++){
-			KEY tmpKeyMasked = ((65 - j) & keyReformed);
-			tmpKeyMasked << PC2[j];
-			keyPC2 += tmpKeyMasked;
+		int j = 0; 
+		for(j = 0; j<64; j++){
+			KEY tmpKeyMasked = ((KEY)pow(2, i) & keyReformed);
+			tmpKeyMasked <<= PC2[j];
+			keyPC2 |= tmpKeyMasked;
 		}
 
 		keysP[i] = keyPC2;
@@ -134,7 +138,7 @@ CIPHER_TEXT encrypt(PLAIN_TEXT plain, KEY key){
 	CIPHER_TEXT ciphered = 0; 
 	for(i = 1; i<65; i++){
 		CIPHER_TEXT tmpPermuted = ((65 - i) & permuted);
-		tmpPermuted << IIP[i];
+		tmpPermuted << IIP[i-1];
 		ciphered += tmpPermuted;
 	}
 	
