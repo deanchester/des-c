8c7856862ef62fe602a3eec3341eaf282290b57c
Dean Chester <dean@maquib.com>
2014-03-11 11:54:49 +0000

    Refactored the left and right for PC1, and alos added a print bits function.

--- DES.c
+++ DES.c
@@ -4,8 +4,9 @@ int PC2[] = {14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,
 			2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,
 			50,36,29,32};
 
-int PC1[] = {57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,
-			11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,
+int PC1_LEFT[] = {57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,
+			11,3,60,52,44,36};
+int PC1_RIGHT[] = {63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,
 			61,53,45,37,29,21,13,5,28,20,12,4};
 
 int IP[] = {58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,
@@ -68,26 +69,50 @@ KEY_LR shiftKey(KEY_LR key){
 	return (key << (unsigned int)1) | (key >> (sizeof(key) * (unsigned int) 8 - (unsigned int) 1));
 }
 
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = b[i] & (1<<j);
+            byte >>= j;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
+
 /*
 	Run the key scheduling algorithm 
 */
 void scheduleKeys(KEY key, KEY *keysP){
-	int i;
+	int i, k =0;
 	KEY keyPC1 = 0;
-	
-	KEY value = 1;
-	for(i = 0; i<64; i++){
-		KEY tmpKeyMasked = (value & key);
-		if(tmpKeyMasked == value){
-			keyPC1 += (1<<(PC1[i]-1));
-		}
-		value *= 2;
-	}
+		
 	KEY_LR key_left = 0;
 	KEY_LR key_right = 0;
-	key_left = ((keyPC1 & 0xFFFFFFFF00000000) >> 32);
-	key_right =  (keyPC1 & 0x00000000FFFFFFFF);
+	
+	for(i =0; i<28; i++){
+		KEY_LR tmp = key & 1<<PC1_RIGHT[i];
+		if(tmp == 1<<PC1_RIGHT[i]){
+			key_right += (1<<i);
+		}
+	}
 
+	for(i =0; i<28; i++){
+		KEY_LR tmp = key & 1<<PC1_LEFT[i];
+		if(tmp == 1<<PC1_LEFT[i]){
+			key_left += (1<<i);
+		}
+	}
+	printf("Left: %x\n", key_left);
+	printf("Right: %x\n", key_right);
+	KEY value = 1;
 	for(i = 0; i<16; i++){
 		if(i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7 || i == 9 || 
 			i == 10 || i == 11 || i == 12 || i == 13 || i == 14) {
