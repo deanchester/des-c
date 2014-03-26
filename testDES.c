52d57899caf82cd3697fa6f6f5594fb4b31ebefa
Dean Chester <dean@maquib.com>
2014-03-26 23:46:20 +0000

    Added the correct example plain text and key.

--- testDES.c
+++ testDES.c
@@ -1,14 +1,15 @@
 #include "DES.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char const *argv[])
 {
-	KEY key = 0; 
-	PLAIN_TEXT pt = 0; 
+	KEY key = (KEY)0x133457799BBCDFF1; 
+	PLAIN_TEXT pt = (PLAIN_TEXT)0x0123456789ABCDEF; 
 	
 	CIPHER_TEXT ct = encrypt(pt, key);
 	PLAIN_TEXT p = decrypt(ct, key);
-	printf("Ciphered Value is: %llu\n", p);
+	printf("Plain Value is: %llx\n", p);
 	
 	return 0;
 }
